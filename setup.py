31a459a2455810ab7ec19f18a7ae34b006ff468b
Riya Rajput <riyarrrajput053@gmail.com>
2025-03-27 13:11:38 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Riya Rajput",
+    author_email="riyarrrajput053@gmail.com",
+    packages=find_packages()
+)
