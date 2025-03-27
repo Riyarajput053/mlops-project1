ebf8100192b7c1e08f8065fa4a09f944410ed4bc
Riya Rajput <riyarrrajput053@gmail.com>
2025-03-27 16:56:57 +0530

    completed pipeline, and pushed to s3 bucket

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -31,35 +31,35 @@ DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
 DATA_INGESTION_INGESTED_DIR:str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.25
 
-# # data validation
-# DATA_VALIDATION_DIR_NAME: str = "data_validation"
-# DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
+# data validation
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
 
-# # data transformation
-# DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
-# DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str = "transformed"
-# DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str = "transformed_object"
+# data transformation
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str = "transformed_object"
 
-# # model trainer
-# MODEL_TRAINER_DIR_NAME:str = "model_trainer"
-# MODEL_TRAINER_TRAINED_MODEL_DIR:str = "trained_model"
-# MODEL_TRAINER_TRAINED_MODEL_NAME:str = "model.pkl"
-# MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-# MODEL_TRAINER_MODEL_CONFIG_FILE_PATH:str= os.pth.join("config","model.yaml" )
-# MODEL_TRAINER_N_ESTIMATORS = 200
-# MODEL_TRAINER_MIN_SAMPLES_SPLIT:int = 7
-# MODEL_TRAINER_MIN_SAMPLES_LEAF:int = 6
-# MIN_SAMPLES_SPLIT_MAX_DEPTH:int = 6
-# MIN_SAMPLES_SPLIT_CRITERION:str = 'entropy'
-# MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
+# model trainer
+MODEL_TRAINER_DIR_NAME:str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME:str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH:str= os.path.join("config","model.yaml" )
+MODEL_TRAINER_N_ESTIMATORS = 200
+MODEL_TRAINER_MIN_SAMPLES_SPLIT:int = 7
+MODEL_TRAINER_MIN_SAMPLES_LEAF:int = 6
+MIN_SAMPLES_SPLIT_MAX_DEPTH:int = 6
+MIN_SAMPLES_SPLIT_CRITERION:str = 'entropy'
+MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 
-# # model evaluation
+# model evaluation
 
-# MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-# MODEL_BUCKET_NAME = ""
-# MODEL_PUSHER_S3_KEY = ""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "mlops-project-demo"
+MODEL_PUSHER_S3_KEY = ""
 
-# APP_HOST = "0.0.0.0"
-# APP_PORT = 5000
+APP_HOST = "0.0.0.0"
+APP_PORT = 5000
 
 
